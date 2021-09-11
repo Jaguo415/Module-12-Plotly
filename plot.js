5af605b3995e1546776ffae136f664289167ba0c
Jaguo415 <83923903+Jaguo415@users.noreply.github.com>
2021-09-11 12:40:58 -0700

    12.3.2

--- plot.js
+++ plot.js
@@ -1,18 +1,42 @@
-var sortedCities = cityGrowths.sort((a,b) => a.Increase_from_2016 - b.Increase_from_2016).reverse(); 
-var topFiveCities = sortedCities.slice(0,5);
-var topFiveCityNames = topFiveCities.map(city => city.City);
-var topFiveCityGrowths = topFiveCities.map(city => parseInt(city.Increase_from_2016));
-var trace = {
-  x: topFiveCityNames,
-  y: topFiveCityGrowths,
-  type: "bar"
-};
-var data = [trace];
-var layout = {
-  title: "Most Rapidly Growing Cities",
-  xaxis: { title: "City" },
-  yaxis: { title: "Population Growth, 2016-2017"}
-};
-Plotly.newPlot("bar-plot", data, layout);
-console.log(cityGrowths);
+// d3.json("samples.json").then(function(data){
+//     console.log(data);
+// });
 
+
+// d3.json("samples.json").then(function(data){
+//     wfreq = data.metadata.map(person => person.wfreq);
+//     console.log(wfreq);
+// });
+
+// d3.json("samples.json").then(function(data){
+//     wfreq = data.metadata.map(person =>
+// person.wfreq).sort((a,b) => b - a);
+//     console.log(wfreq);
+// });
+
+// d3.json("samples.json").then(function(data){
+//     wfreq = data.metadata.map(person =>
+// person.wfreq).sort((a,b) => b - a);
+//     filteredWfreq = wfreq.filter(element => element !=
+// null);
+//     console.log(filteredWfreq);
+// });
+
+// researcher1 = {
+//   name: 'Roza',
+//   age: 34,
+//   hobby: 'Hiking'
+// };
+// console.log(Object.entries(researcher1));
+
+
+// for each loops
+researcher1 = [['name', 'Roza'], ['age', 34], ['hobby', 'Hiking']];
+researcher1.forEach(([first, second]) => console.log(first + ": " + second));
+
+// Using object.entries
+d3.json("samples.json").then(function(data){
+  firstPerson = data.metadata[0];
+  Object.entries(firstPerson).forEach(([key, value]) =>
+    {console.log(key + ': ' + value);});
+});
